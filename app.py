1f207dc158699949c62f772269f2ada1e08111d0
Henry <hnryzhng@gmail.com>
2020-02-22 10:18:06 +0800

    correct app error

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def send_sms():
 		# message parameters
 		senderName = request.form['sender-name']
 		fromPhone= os.environ['FROM_PHONE']
-		toPhone= request.form['recipient-phone']	# validate phone number using Twilio service?
+		toPhone= '+17133675645'	# validate phone number using Twilio service?
 		messageBody = request.form['message'] + f"- 'Hi {senderName}, Henry can help you send text messages like this one for customer notifications or surveys.' "
 
 		# set message parameters
